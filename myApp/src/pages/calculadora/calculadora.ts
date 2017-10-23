dca77ca9c03f2dbfa8dda3484696520717e5700b
Edwin Lima <edwin.limaa@gmail.com>
2017-10-23 13:02:15 -0200

    Update calculadora.ts
    Retirando comentarios

--- myApp/src/pages/calculadora/calculadora.ts
+++ myApp/src/pages/calculadora/calculadora.ts
@@ -77,53 +77,7 @@ export class CalculadoraPage {
 	 		this.historico.push(this.results);
 	 	}
 	 }
-	 /*
-	 public calculoPrestacao(): void {
-
-		 if(this.meses == 0 || this.meses == undefined){
-		   let alert = this.alertCtrl.create({
-		     title: 'Campo em Branco',
-		     subTitle: 'Preencha o número de Parcelas',
-		     buttons: ['OK']
-		   });
-		   alert.present();
-		 }
-		 else if(this.juros == 0 || this.juros == undefined){
-		     let alert = this.alertCtrl.create({
-		       title: 'Campo em Branco',
-		       subTitle: 'Preencha o valor dos Juros',
-		       buttons: ['OK']
-		     });
-		     alert.present();
-		 }
-		 else if(this.prestacao == 0 || this.prestacao == undefined){
-		     let alert = this.alertCtrl.create({
-		       title: 'Campo em Branco',
-		       subTitle: 'Preencha o valor da Prestação',
-		       buttons: ['OK']
-		     });
-		     alert.present();
-		 }
-		 else {
-		     this.meses = Number(this.meses); 
-		     this.juros = Number(this.juros);
-		     this.prestacao = Number(this.prestacao);  
-		     if(this.isAbs == false){
-		       this.juros = this.juros/100;
-		       this.isAbs = true;
-		     }
-		     this.results = (( 1- (1  + this.juros )** -this.meses) / this.juros) * this.prestacao;
-		     this.results = this.results.toFixed(3);
-		     if(this.historico.length >= 2*4){
-		       this.historico = this.historico.splice(4);
-		     }
-		     this.historico.push(this.meses);
-		     this.historico.push(this.juros);
-		     this.historico.push(this.prestacao);
-		     this.historico.push(this.results);
-		}
-	 }*/
-
+	
 	 public reset() {
 	   this.meses = ''; 
 	   this.juros = '';
